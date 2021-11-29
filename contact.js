3f3a2823e9b555893e3278a486c302d1468e9df1
Maor <maorsa9@gmail.com>
2021-11-29 13:50:17 +0200

    update contact

--- contact.js
+++ contact.js
@@ -1,5 +1,7 @@
 function send_email(){
 var x = document.getElementById("message").value;
-window.open('mailto:Tripplane@gmail.com?subject=subject'+ "&body="+encodeURIComponent(x));
+var y = document.getElementById("name").value;
+var z = document.getElementById("number").value;
+window.open('mailto:Tripplane@gmail.com?subject=subject'+ "&body="+encodeURIComponent(x + "\n User name: " + y + "\n Phone number: " + z ));
 }
 
